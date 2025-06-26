fa6dbc6ec7d8024fe737611c50131cb6d9072bb2
MiguelBandeira21 <bandeira.miguel2002@gmail.com>
2025-06-26 17:32:29 +0100

    Few changes

---
+++ gitRun.py
@@ -0,0 +1,57 @@
+import windtunnel
+import inductiva
+
+
+project = inductiva.projects.Project(
+   name="UAV_dataset")
+
+# Allocate cloud machine
+cloud_machine = inductiva.resources.MachineGroup(
+   provider="GCP",
+   machine_type="c2d-highcpu-32",
+   spot=True
+)
+
+tasks = []
+DATASET_SIZE = 41
+alpha = -20 # Angle of attack
+
+for i in range(DATASET_SIZE):
+    
+    # Create wind tunnel object
+    wind_tunnel = windtunnel.WindTunnel(dimensions=(20,10,8))
+
+    # Set the object in the windtunnel
+    wind_tunnel.set_object(
+        object_path="assets/orientedUAV.obj",
+        rotate_z_degrees=0,
+        rotate_y_degrees=alpha,
+        normalize=False,
+        center=True,
+    )
+
+    # Display the Windtunnel with the object
+    # wind_tunnel.display()
+
+    print("Starting UAV Simulation")
+    # Submit a simulation task
+    task = wind_tunnel.simulate(wind_speed_ms=20,
+                                num_iterations=50,
+                                resolution=3, 
+                                machine_group_name=cloud_machine.name)
+    
+    project.add_task(task)
+    
+    task.wait() 
+
+    alpha += 1
+    
+    
+   
+for task, alpha in tasks:
+    output = task.get_output()  # waits until done
+    task.set_metadata({"angle_of_attack": str(alpha)})
+
+project.wait()
+cloud_machine.terminate()
+
--- windtunnel/windtunnel.py
+++ windtunnel/windtunnel.py
@@ -90,6 +90,7 @@ class WindTunnel:
     def set_object(self,
                    object_path: str,
                    rotate_z_degrees: float = 0,
+                   rotate_y_degrees: float = 0,
                    normalize: bool = True,
                    center: bool = True):
         """Load an object into the windtunnel scenario. Optionally rotates,
@@ -113,6 +114,8 @@ class WindTunnel:
             mesh, displace_vector, = pre_processing.move_mesh_to_origin(mesh)
         if rotate_z_degrees:
             mesh = mesh.rotate_z(rotate_z_degrees)
+        if rotate_y_degrees:
+            mesh = mesh.rotate_y(rotate_y_degrees)
         if normalize:
             max_object_dimensions = (self.length * MAX_OBJECT_LENGTH_FACTOR,
                                      self.width * MAX_OBJECT_WIDTH_FACTOR,
